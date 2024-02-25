b7ad21f56ce4f3e08b638a858cceab7e40426541
RITESHP36 <codingritesh36@gmail.com>
2024-02-25 23:48:24 +0530

    Fix database connection retries and add timeout limit

--- backend/index.js
+++ backend/index.js
@@ -14,21 +14,31 @@ const commentRoute = require("./routes/comments");
 //database
 let isDatabaseConnected = false;
 const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URL);
-		console.log("database is connected successfully!");
-		isDatabaseConnected = true;
-	} catch (err) {
-		console.log(err);
-		isDatabaseConnected = false;
+	let retries = 5;
+	while (retries) {
+		try {
+			await mongoose.connect(process.env.MONGO_URL);
+			console.log("database is connected successfully!");
+			isDatabaseConnected = true;
+			break; // Exit the loop if connected successfully
+		} catch (err) {
+			console.log("Error connecting to database:", err);
+			retries--;
+			console.log(`Retrying connection. ${retries} retries left...`);
+			await new Promise((res) => setTimeout(res, 5000)); // Wait for 5 seconds before retrying
+		}
+	}
+	if (!retries) {
+		console.log("Failed to connect to database after retries.");
 	}
 };
+
 
 //middlewares
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: ["http://localhost:5173", "*"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","*"], credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -38,27 +48,29 @@ app.use("/api/comments", commentRoute);
 // Set timeout limit to 10 seconds
 app.use((req, res, next) => {
 	res.setTimeout(10000, () => {
-		console.log("Request timed out");
-		res.status(504).send("Request timed out");
+	  console.log("Request timed out");
+	  res.status(504).send("Request timed out");
 	});
 	next();
-});
+  });
 
 // Welcome route in JSON format
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to the MERN Blogging Website!" });
-});
-
+  });
+  
 // Check if the database is connected and send JSON response
 app.get("/check-db-status", (req, res) => {
-	if (isDatabaseConnected) {
-		res.json({ status: "Database connected" });
-	} else {
-		res.json({ status: "Database not connected" });
-	}
+  if (isDatabaseConnected) {
+    res.json({ status: "Database connected" });
+  } else {
+    res.json({ status: "Database not connected" });
+  }
 });
 
-app.listen(() => {
+
+
+app.listen(process.env.PORT, () => {
 	connectDB();
-	console.log("app is running ");
+	console.log("app is running on port " + process.env.PORT);
 });
